76b14125aa08df646d802a3087f7219d977e0a98
Rupesh-Singh-Karki <karkisinghleetcode@gmail.com>
2025-08-14 23:15:24 +0530

    frontend setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+
+
+function App() {  
+
+  return (
+    <div className="flex items-center justify-center bg-gray-100"  >
+      <h1 className="text-3xl font-bold ">Hello jii</h1>
+    </div>
+  )
+}
+
+export default App
